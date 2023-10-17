f3a9a0fee91d32ca2a3c63726f23e9353d2a68df
Mommy Me <mkm0182@yahoo.com>
2023-10-17 12:05:15 -0500

    Updated schema.sql

--- Main/db/schema.sql
+++ Main/db/schema.sql
@@ -11,14 +11,18 @@ CREATE TABLE departments (
 CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     role_id VARCHAR(30),
-    salary TEXT NOT NULL
+    salary DECIMAL(10,2),
+    department_id INT,
+    FOREIGN KEY (department_id),
+    REFERENCES departments(id),
+    ON DELETE SET NULL
 );
 
 CREATE TABLE employees (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     role_id INT,
-    manager_id INT
+    manager_id INT NOT NULL
 );
 
